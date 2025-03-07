47096b1ba0909f5a9ad1239a36a6983f44a3a57e
SanjayAsokan <162708826+SanjayAsokan@users.noreply.github.com>
2025-03-07 21:28:37 +0530

    Add files via upload

---
+++ problem13.js
@@ -0,0 +1,21 @@
+let array = ["playing","reading","homework","exeicise","yogo"]
+console.log("Task ===>",array)
+
+//remove 1st Task
+let arr = []
+for(let i = 1 ;i < array.length ;i++)
+{
+  arr[i-1]=array[i]
+}
+console.log("First Task is removed ==>",arr)
+
+// Add 2 Tasks from Starting
+arr1 = ["jack","jump"]
+for(let j =0; j<arr.length;j++)
+{
+  arr1[j+2] = arr[j]
+}
+console.log("Two New Tasks Added ==>",arr1)
+
+arr1[arr1.length-1]=2 
+console.log(arr1)
